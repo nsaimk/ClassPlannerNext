2ba96d98f2692f5ba57a6badbd95b86b68a10ea2
batuncer <tuncerbaki@gmail.com>
2023-11-09 14:46:53 +0000

    server testing

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express")
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
-const port = process.env.PORT||5000;
+const cors = require("cors");
+const port = process.env.PORT || 10000;
 
-app.listen(port, ()=> {
-    console.log(`server running on port ${port}`)
-})
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from the server!</h1>");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
