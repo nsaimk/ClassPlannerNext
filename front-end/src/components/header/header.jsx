c5010b59a113cd271243734b6bb7e8e8c3c6735d
nsaimk <nsaimkorkmaz1@gmail.com>
2024-02-24 13:39:28 +0000

    reminder added

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <AppBar position="fixed" >
         <Toolbar style={{ backgroundColor: "white"}}>
           <img
-            src="https://ih1.redbubble.net/image.2953589498.1437/st,small,507x507-pad,600x600,f8f8f8.jpg" 
+            src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png" 
             style={{ height: '90px', margin: '5px 0px' }}
           />
           <Typography variant="h2"
